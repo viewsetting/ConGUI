547a2ace97d6aaea066056b248fe812685c80689
viewsetting <viewsetting@Gmail.com>
2017-08-10 08:56:50 +0800

    C++  codes
    THIS  IS IT.

---
+++ animation.h
@@ -0,0 +1,32 @@
+/*
+This project is designed for C++ class design.By drawing simple lines,you can create wonderful
+effect of GUI in consolelation.
+WARNING:ONLY DESIGNED FOR WIN 10,WIN 8,Win 7,Win Vista,Win XP, etc.
+Developed by viewsetting Jan,2017
+Released in Aug,2017
+*/
+#ifndef ANIMATION_H_INCLUDED
+#define ANIMATION_H_INCLUDED
+#include "windows.h"
+#include<iostream>
+#include<string>
+using namespace std;
+class animation
+{
+public:
+    void show_dot( int spl,int dots,int loop, string head="loading");
+    void rotatesign ( int spcs,int dots,string head);
+    void processbar_tra(int len,int spd,char style_bar='-',string style_conti="■",char style_verticle='|',int lenconti=2);
+    void processbar_winxp(int len,int spd,char style_bar='-',char style_verticle='|',int lenchunk=2,string style_conti="■" );
+    animation(int x=1,int y=1);
+    ~animation();
+    void change(int x,int y);
+    void show_dot_gotoxy( int spl, int dots,int loop,int x,int y,string head="Loading");
+   // animation();
+    void gotoxy(int x,int y);
+    void freshcord();
+    int posx;
+    int posy;
+};
+
+#endif // ANIMATION_H_INCLUDED
---
+++ framework.h
@@ -0,0 +1,24 @@
+/*
+This project is designed for C++ class design.By drawing simple lines,you can create wonderful
+effect of GUI in consolelation.
+WARNING:ONLY DESIGNED FOR WIN 10,WIN 8,Win 7,Win Vista,Win XP, etc.
+Developed by viewsetting Jan,2017
+Released in Aug,2017
+*/
+#ifndef FRAMEWORK_H_INCLUDED
+#define FRAMEWORK_H_INCLUDED
+#include<string>
+using namespace std;
+void basic_frame(int x=1,int y=1,int height=10,int width=40,char log='-',char ver='|');
+void gotoxy(int x,int y);
+void caption(int x,int y,string inputstr);
+void clean_line(int xstart,int ystart,int len);
+void clean_line_row(int xstart,int ystart,int len);
+void clean_area(int x,int y,int width,int height);
+void draw_column(int xstart,int  ystart,int len,char op='|');
+void draw_row(int xstart,int ystart,int len,char op='-');
+void msgboxer(int x,int y,string inform,int linewidth,int height,int width,int blankline);
+void move_frame(int x,int y,string inputstr,int speed_per_char=100,int len=110,int height=1,char style_log='-',char style_ver='|');
+string psdbox(int x,int y);
+void draw_grid(int xstart,int ystart ,int length,int height);
+#endif // FRAMEWORK_H_INCLUDED
